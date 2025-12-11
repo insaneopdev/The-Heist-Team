68122eabcd22102d5a3d1af90c2f7550ff48fa71
hipstarVenom <yuvansakthi046@gmail.com>
2025-12-11 22:41:32 +0530

    removed wall_impact

--- Scripts/Bullet.gd
+++ Scripts/Bullet.gd
@@ -3,7 +3,6 @@ extends Area3D
 #BULLET SETTINGS
 @export var bullet_speed = 30.0
 @export var life_time := 2.0
-@export var impact_scene: PackedScene
 
 #OTHER VARIABLES
 var direction: Vector3 = Vector3.ZERO
@@ -28,22 +27,7 @@ func _physics_process(delta):
 	var result = space_state.intersect_ray(query)
 
 	if result:
-		var hit_pos = result.position
-		var hit_norm = result.normal
-
-		var impact = impact_scene.instantiate()
-		get_tree().current_scene.add_child(impact)
-
-		impact.global_position = hit_pos + hit_norm * 0.05
-
-		var up_dir = Vector3.UP
-		if abs(hit_norm.dot(Vector3.UP)) > 0.9:
-			up_dir = Vector3.FORWARD
-
-		impact.look_at(hit_pos + hit_norm, up_dir)
-		impact.scale = Vector3(0.1, 0.1, 0.1)
-
-		get_tree().create_timer(0.1).timeout.connect(impact.queue_free)
+		
 		queue_free()
 		return
 
