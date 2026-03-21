19adb02a710999987258fd4a8e6df70e8011d8db
Insane Op <raghulsah199@gmail.com>
2026-03-21 11:02:44 +0530

    Slot Machine : Update

---
+++ Scripts/casino.gd
@@ -0,0 +1,9 @@
+extends Node3D
+
+
+func _on_area_3d_body_entered(body: Node3D) -> void:
+	pass # Replace with function body.
+
+
+func _on_drilltime_timeout() -> void:
+	pass # Replace with function body.
